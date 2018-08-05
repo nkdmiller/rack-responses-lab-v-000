d95f8ca12cc75ecf02a25753522f28574c90dd25
Nate Miller <natekmiller96@gmail.com>
2018-08-05 01:50:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,11 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    resp.write Time.now
- 
+    if resp.write Time.now > 12
+      resp.write "o"
+    else
+      resp.write "l"
+    end
     resp.finish
   end
  
